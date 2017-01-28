5713fcb4e879fa02eefec12e9312b4713b515a6e
Enos Vazquez <enosvazquez@gmail.com>
2017-01-28 21:15:25 +0000

    fixed bug that allowed any user to update

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -26,6 +26,10 @@ class PlacesController < ApplicationController
 
     def update
         @place = Place.find(params[:id])
+        if @place.user != current_user
+            return render text: 'Not Allowed', status: :forbidden
+        end
+
         @place.update_attributes(place_params)
         redirect_to root_path
     end
