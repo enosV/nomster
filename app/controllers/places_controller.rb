2e8816cd7b7eb618c39709e75fc330cdccad366b
Enos Vazquez <enosvazquez@gmail.com>
2017-02-01 04:17:30 +0000

    prevent blank names

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -33,13 +33,18 @@ class PlacesController < ApplicationController
     end
 
     def update
-        @place = Place.find(params[:id])
-        if @place.user != current_user
-            return render text: 'Not Allowed', status: :forbidden
-        end
+      @place = Place.find(params[:id])
 
-        @place.update_attributes(place_params)
+      if @place.user != current_user
+        return render text: 'Not Allowed', status: :forbidden
+      end
+
+      @place.update_attributes(place_params)
+      if @place.valid?
         redirect_to root_path
+      else
+        render :edit, status: :unprocessable_entity
+      end
     end
 
     def destroy
